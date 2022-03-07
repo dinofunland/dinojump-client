576d9e1c313e97a1b99ba82ca546e925a087ac81
Wenish <Wenish@users.noreply.github.com>
2022-03-07 21:51:56 +0100

    update schams

--- Assets/Schemas/GameSchema.cs
+++ Assets/Schemas/GameSchema.cs
@@ -2,10 +2,11 @@
 // THIS FILE HAS BEEN GENERATED AUTOMATICALLY
 // DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
 // 
-// GENERATED USING @colyseus/schema 1.0.34
+// GENERATED USING @colyseus/schema 2.0.4
 // 
 
 using Colyseus.Schema;
+using Action = System.Action;
 
 namespace Dinojump.Schemas {
 	public partial class GameSchema : Schema {
@@ -23,5 +24,75 @@ namespace Dinojump.Schemas {
 
 		[Type(4, "ref", typeof(FloorSchema))]
 		public FloorSchema floor = new FloorSchema();
+
+		/*
+		 * Support for individual property change callbacks below...
+		 */
+
+		protected event PropertyChangeHandler<string> _gameStepChange;
+		public Action OnGameStepChange(PropertyChangeHandler<string> handler) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(gameStep));
+			_gameStepChange += handler;
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(gameStep));
+				_gameStepChange -= handler;
+			};
+		}
+
+		protected event PropertyChangeHandler<float> _gameSpeedChange;
+		public Action OnGameSpeedChange(PropertyChangeHandler<float> handler) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(gameSpeed));
+			_gameSpeedChange += handler;
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(gameSpeed));
+				_gameSpeedChange -= handler;
+			};
+		}
+
+		protected event PropertyChangeHandler<MapSchema<PlayerSchema>> _playersChange;
+		public Action OnPlayersChange(PropertyChangeHandler<MapSchema<PlayerSchema>> handler) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(players));
+			_playersChange += handler;
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(players));
+				_playersChange -= handler;
+			};
+		}
+
+		protected event PropertyChangeHandler<MapSchema<PlatformSchema>> _platformsChange;
+		public Action OnPlatformsChange(PropertyChangeHandler<MapSchema<PlatformSchema>> handler) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(platforms));
+			_platformsChange += handler;
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(platforms));
+				_platformsChange -= handler;
+			};
+		}
+
+		protected event PropertyChangeHandler<FloorSchema> _floorChange;
+		public Action OnFloorChange(PropertyChangeHandler<FloorSchema> handler) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(floor));
+			_floorChange += handler;
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(floor));
+				_floorChange -= handler;
+			};
+		}
+
+		protected override void TriggerFieldChange(DataChange change) {
+			switch (change.Field) {
+				case nameof(gameStep): _gameStepChange?.Invoke((string) change.Value, (string) change.PreviousValue); break;
+				case nameof(gameSpeed): _gameSpeedChange?.Invoke((float) change.Value, (float) change.PreviousValue); break;
+				case nameof(players): _playersChange?.Invoke((MapSchema<PlayerSchema>) change.Value, (MapSchema<PlayerSchema>) change.PreviousValue); break;
+				case nameof(platforms): _platformsChange?.Invoke((MapSchema<PlatformSchema>) change.Value, (MapSchema<PlatformSchema>) change.PreviousValue); break;
+				case nameof(floor): _floorChange?.Invoke((FloorSchema) change.Value, (FloorSchema) change.PreviousValue); break;
+				default: break;
+			}
+		}
 	}
 }
--- Assets/Schemas/InputSchema.cs
+++ Assets/Schemas/InputSchema.cs
@@ -2,10 +2,11 @@
 // THIS FILE HAS BEEN GENERATED AUTOMATICALLY
 // DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
 // 
-// GENERATED USING @colyseus/schema 1.0.34
+// GENERATED USING @colyseus/schema 2.0.4
 // 
 
 using Colyseus.Schema;
+using Action = System.Action;
 
 namespace Dinojump.Schemas {
 	public partial class InputSchema : Schema {
@@ -14,5 +15,39 @@ namespace Dinojump.Schemas {
 
 		[Type(1, "boolean")]
 		public bool right = default(bool);
+
+		/*
+		 * Support for individual property change callbacks below...
+		 */
+
+		protected event PropertyChangeHandler<bool> _leftChange;
+		public Action OnLeftChange(PropertyChangeHandler<bool> handler) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(left));
+			_leftChange += handler;
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(left));
+				_leftChange -= handler;
+			};
+		}
+
+		protected event PropertyChangeHandler<bool> _rightChange;
+		public Action OnRightChange(PropertyChangeHandler<bool> handler) {
+			if (__callbacks == null) { __callbacks = new SchemaCallbacks(); }
+			__callbacks.AddPropertyCallback(nameof(right));
+			_rightChange += handler;
+			return () => {
+				__callbacks.RemovePropertyCallback(nameof(right));
+				_rightChange -= handler;
+			};
+		}
+
+		protected override void TriggerFieldChange(DataChange change) {
+			switch (change.Field) {
+				case nameof(left): _leftChange?.Invoke((bool) change.Value, (bool) change.PreviousValue); break;
+				case nameof(right): _rightChange?.Invoke((bool) change.Value, (bool) change.PreviousValue); break;
+				default: break;
+			}
+		}
 	}
 }
