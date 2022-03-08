a272f500ed17bc66a41bfbd7744fe1cf1e54adb0
Wenish <Wenish@users.noreply.github.com>
2022-03-08 15:02:53 +0100

    feat(InputManager): only send inputs to server when changed

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -11,34 +11,54 @@ public class InputManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    
+
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        jumpInput = Input.GetButton("Jump");
-        modulationInput = Input.GetButton("Fire3");
-        if (RoomManager.Instance?.colyseusRoom != null)
+        var newHorizontalInput = Input.GetAxis("Horizontal");
+        var hasHorizontalInputChanged = newHorizontalInput != horizontalInput;
+        if (hasHorizontalInputChanged)
         {
+            horizontalInput = newHorizontalInput;
             SendPLayerMovement();
         }
+
+        var newJumpInput = Input.GetButton("Jump");
+        var hasJumpInputChanged = newJumpInput != jumpInput;
+        if (hasJumpInputChanged)
+        {
+            jumpInput = newJumpInput;
+            if(jumpInput) {
+                SendPlayerJump();
+            }
+        }
+
+        // TODO handle modulationInput
+        modulationInput = Input.GetButton("Fire3");
     }
 
     async void SendPLayerMovement()
     {
-        if (horizontalInput == 0)
-            await RoomManager.Instance.colyseusRoom.Send("move", new { left = false, right = false });
-        if (horizontalInput > 0)
-            await RoomManager.Instance.colyseusRoom.Send("move", new { left = false, right = true });
-        if (horizontalInput < 0)
-            await RoomManager.Instance.colyseusRoom.Send("move", new { left = true, right = false });
-        if (jumpInput)
+        if (RoomManager.Instance?.colyseusRoom != null)
+        {
+            if (horizontalInput == 0)
+                await RoomManager.Instance.colyseusRoom.Send("move", new { left = false, right = false });
+            if (horizontalInput > 0)
+                await RoomManager.Instance.colyseusRoom.Send("move", new { left = false, right = true });
+            if (horizontalInput < 0)
+                await RoomManager.Instance.colyseusRoom.Send("move", new { left = true, right = false });
+        }
+    }
+
+    async void SendPlayerJump()
+    {
+        if (RoomManager.Instance?.colyseusRoom != null)
+        {
             await RoomManager.Instance.colyseusRoom.Send("jump");
-        if (modulationInput)
-            await RoomManager.Instance.colyseusRoom.Send("mod");
+        }
     }
 }
