d07770ee9b348161a6ebf12b5a8e44367215ca0c
Wenish <Wenish@users.noreply.github.com>
2022-03-09 19:23:19 +0100

    adding camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour
-{
+public class CameraController : MonoBehaviour
+{    
     public bool isFollowingPlayer = true;
     // Start is called before the first frame update
     void Start()
