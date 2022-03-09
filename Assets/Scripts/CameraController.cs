62ad38c37672a9530692e0b6687b1e066b6f928e
Wenish <Wenish@users.noreply.github.com>
2022-03-10 00:59:50 +0100

    adding camera following player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CameraController : MonoBehaviour
-{    
-    public bool isFollowingPlayer = true;
+{
+    public Vector3 OffsetCamera;
+    public float SpeedCamera;
+    public bool IsFocusingPlayer = true;
+    public Transform CameraTarget;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsFocusingPlayer)
+        {
+            var playerControllers = GameObject.FindObjectsOfType<PlayerController>();
+            var playerController = playerControllers.FirstOrDefault(playerController => playerController.playerKey == GameManager.Instance.myPlayerKey);
+            if (playerController)
+            {
+                CameraTarget = playerController.gameObject.transform;
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (IsFocusingPlayer)
+        {
+            if (CameraTarget != null)
+            {
+                var desiredPosition = CameraTarget.position + OffsetCamera;
+                var t = Time.deltaTime / SpeedCamera;
+                transform.position = Vector3.Lerp(transform.position, desiredPosition, t);
+            }
+
+        }
     }
 }
