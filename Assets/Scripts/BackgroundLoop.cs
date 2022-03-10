48062ed91be5ad3d1a9c26f6ac54adf254a0ac2b
Simon <404devops@gmail.com>
2022-03-11 00:52:23 +0100

    LoopWalk Animation & BackgroundLoop Added

--- Assets/Scripts/BackgroundLoop.cs
+++ Assets/Scripts/BackgroundLoop.cs
@@ -10,7 +10,7 @@ public class BackgroundLoop : MonoBehaviour
     private Camera mainCamera;
     private Vector2 screenBounds;
     public float choke;
-    // Start is called before the first frame update
+
     void Start()
     {
         mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
@@ -20,7 +20,7 @@ public class BackgroundLoop : MonoBehaviour
 
     void LoadChildObjects(GameObject obj)
     {
-        float objectWidth = obj.GetComponent<SpriteRenderer>().bounds.size.x -choke;
+        float objectWidth = obj.GetComponent<SpriteRenderer>().bounds.size.x - choke;
         int childsNeeded = (int)Mathf.Ceil(screenBounds.x * 2 / objectWidth);
         GameObject clone = Instantiate(obj);
         for (int i = 0; i <= childsNeeded; i++)
@@ -35,7 +35,6 @@ public class BackgroundLoop : MonoBehaviour
         Destroy(obj.GetComponent<SpriteRenderer>());
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         RepositionChildObjects(background);
@@ -44,11 +43,11 @@ public class BackgroundLoop : MonoBehaviour
     private void RepositionChildObjects(GameObject obj)
     {
         Transform[] children = obj.GetComponentsInChildren<Transform>();
-        if (children.Length > 1) 
+        if (children.Length > 1)
         {
             GameObject firstChild = children[1].gameObject;
             GameObject lastChild = children[^1].gameObject;
-            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x -choke;
+            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x - choke;
 
             if (mainCamera.gameObject.transform.position.x + screenBounds.x > lastChild.transform.position.x + halfObjectWidth)
             {
@@ -60,7 +59,6 @@ public class BackgroundLoop : MonoBehaviour
                 lastChild.transform.SetAsFirstSibling();
                 lastChild.transform.position = new Vector3(firstChild.transform.position.x - halfObjectWidth * 2, firstChild.transform.position.y, firstChild.transform.position.z);
             }
-
             //foreach (var child in children)
             //{
             //    RepositionChildObjectsHeight(child.gameObject);
@@ -77,8 +75,8 @@ public class BackgroundLoop : MonoBehaviour
         {
             GameObject c = Instantiate(clone);
             c.transform.SetParent(obj.transform);
-            c.transform.position = new Vector3(objectHeight * i, obj.transform.position.y, obj.transform.position.z);
-            c.name = obj.name + "_y_"+ i;
+            c.transform.position = new Vector3(obj.transform.position.x,objectHeight * i, obj.transform.position.z);
+            c.name = obj.name + "_y_" + i;
         }
         Destroy(clone);
         Destroy(obj.GetComponent<SpriteRenderer>());
@@ -96,12 +94,12 @@ public class BackgroundLoop : MonoBehaviour
             if (mainCamera.gameObject.transform.position.y + screenBounds.y > lastChild.transform.position.y + halfObjectHeight)
             {
                 firstChild.transform.SetAsLastSibling();
-                firstChild.transform.position = new Vector3(lastChild.transform.position.y + halfObjectHeight * 2, lastChild.transform.position.y, lastChild.transform.position.z);
+                firstChild.transform.position = new Vector3(lastChild.transform.position.x, lastChild.transform.position.y + halfObjectHeight * 2, lastChild.transform.position.z);
             }
             else if (mainCamera.gameObject.transform.position.y - screenBounds.y < firstChild.transform.position.y - halfObjectHeight)
             {
                 lastChild.transform.SetAsFirstSibling();
-                lastChild.transform.position = new Vector3(firstChild.transform.position.y - halfObjectHeight * 2, firstChild.transform.position.y, firstChild.transform.position.z);
+                lastChild.transform.position = new Vector3(firstChild.transform.position.x, firstChild.transform.position.y - halfObjectHeight * 2, firstChild.transform.position.z);
             }
         }
     }
