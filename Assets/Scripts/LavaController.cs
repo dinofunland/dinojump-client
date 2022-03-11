3a0e38c06132f1871c4a1d9a11f2804523f062d9
Simon <404devops@gmail.com>
2022-03-11 17:26:24 +0100

    Lava Placement

--- Assets/Scripts/LavaController.cs
+++ Assets/Scripts/LavaController.cs
@@ -6,11 +6,14 @@ using Dinojump.Schemas;
 public class LavaController : MonoBehaviour
 {
     public FloorSchema floorSchema { get; set; }
-    float lerpSpeed;
+    float lerpSpeed = 0.02f;
+
+    float spriteSize;
+    float offset =  2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteSize = GetComponent<SpriteRenderer>().bounds.extents.y;
     }
 
     // Update is called once per frame
@@ -18,8 +21,9 @@ public class LavaController : MonoBehaviour
     {
         if (floorSchema?.position != null)
         {
+            var yPosOffset = spriteSize - offset;
             var t = Time.deltaTime / lerpSpeed;
-            Vector2 desiredPostion = new Vector3(floorSchema.position.x, floorSchema.position.y);
+            Vector2 desiredPostion = new Vector3(floorSchema.position.x, floorSchema.position.y - yPosOffset);
             transform.position = Vector2.Lerp(transform.position, desiredPostion, t);
         }
     }
@@ -28,9 +32,9 @@ public class LavaController : MonoBehaviour
     {
         if (floorSchema != null)
         {
-            Gizmos.color = Color.red;
+            Gizmos.color = Color.green;
             Vector3 size = new Vector3(floorSchema.size.width, floorSchema.size.height);
-            Gizmos.DrawWireCube(this.gameObject.transform.position, size);
+            Gizmos.DrawWireCube(new Vector2(floorSchema.position.x, floorSchema.position.y), size);
         }
     }
 }
